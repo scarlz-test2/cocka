b60d6698a4c742a3d14cb5df61705679c290b411
scarlz <ben@ripley.uk>
2014-09-11 03:51:43 +0100

    removed semi-colon

---
+++ hello.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  var http = require('http');
+
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+
+  }).listen(1337, '127.0.0.1');
+
+  console.log('loser');
+  console.log('Server running at http://127.0.0.1:1337/');
+
+})();
+
